eaedaa39fa0b687339f82f48112af88bfe3b6c53
baolam <lam01662052827@gmail.com>
2025-04-03 20:06:57 +0700

    Read and write file binary

--- src/main.c
+++ src/main.c
@@ -3,12 +3,29 @@
 #include "bookinfor.h"
 #include "stdbool.h"
 
+const char *output_file = "hello_world.bin";
+
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
-    Book book1 = generate_book(1);
-    show_book(book1);
+    FILE *f = fopen(output_file, "rb");
+    // for (int id = 1; id <= 10; id++)
+    // {
+    //     Book book = generate_book(id);
+    //     long offset = ftell(f);
+    //     size_t length = fwrite(&book, sizeof(Book), 1, f);
+
+    //     printf("Book ID: %d | Offset: %ld | Length: %zu bytes\n", id, offset, length);
+    // }
+
+    Book book;
+    fseek(f, 1120, SEEK_SET);
+    fread(&book, sizeof(Book), 1, f);
+
+    show_book(book);
+
+    fclose(f);
 
     return 0;
 }
