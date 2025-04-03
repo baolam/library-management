1244bba057ea9e0bc010a65d6a1f038891ac4bd5
baolam <lam01662052827@gmail.com>
2025-04-03 19:58:30 +0700

    Changing file structture

--- src/main.c
+++ src/main.c
@@ -1,27 +1,14 @@
 #include "gui.h"
 #include "bplustree.h"
+#include "bookinfor.h"
 #include "stdbool.h"
-
-const char *filepath = "E:/library-management/fake_information/books.txt";
-
-struct
-{
-    char bookId[20];
-    char title[100];
-    char author[100];
-    char genre[50];
-    int publicationYear;
-    int stock;
-} Book;
 
 int main(int argc, char *argv[])
 {
-    FILE *f = fopen(filepath, "r");
-    if (f == NULL)
-    {
-        printf("Error reading file!");
-        return 0;
-    }
+    srand(time(NULL));
+
+    Book book1 = generate_book(1);
+    show_book(book1);
 
     return 0;
 }
