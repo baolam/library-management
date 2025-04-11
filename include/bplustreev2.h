403e908a77eedf9d72dc886959aac2ce6acfaaad
baolam <lam01662052827@gmail.com>
2025-04-11 21:10:57 +0700

    Add comment for bplustreev2.h

--- include/bplustreev2.h
+++ include/bplustreev2.h
@@ -1,11 +1,17 @@
-/// Program depends on https://www.programiz.com/dsa/b-plus-tree
+/***
+ * @file bplustreev2.h
+ * @brief Những cài đặt cần thiết để khai thác cây B+ tree
+ * @author Nguyễn Đức Bảo Lâm
+ * @note Nội dung file có dựa trên https://www.programiz.com/dsa/b-plus-tree, chân thành cảm ơn!
+ * @version 1.0
+ */
 
 #ifndef BPLUSTREEV2_H
 #define BPLUSTREEV2_H
 
-#define PRINT_TREE
-#define ORDER 10
-#define MAX_FILE_NAME_LENGTH 128
+#define PRINT_TREE               //> Định nghĩa có cho phép In cây ra hay không, màn hình console
+#define ORDER 10                 //> Số dữ liệu quản lí tối đa của một node
+#define MAX_FILE_NAME_LENGTH 128 //> Độ dài tối đa của tên file
 
 #ifdef PRINT_TREE
 #include <stdio.h>
@@ -18,34 +24,94 @@
 /// @brief bản ghi dữ liệu
 typedef struct Record
 {
-    char _from[MAX_FILE_NAME_LENGTH];
-    long offset;  // Vị trí bắt đầu lưu trữ
-    long length;  // Độ dài dữ liệu lưu trữ
-    bool deleted; // Trạng thái xoá hay chưa (xoá quản lí, cho phương thức soft-delete)
+    char _from[MAX_FILE_NAME_LENGTH]; //> file nội dung lưu trữ
+    long offset;                      //> Vị trí bắt đầu lưu trữ
+    long length;                      //> Độ dài dữ liệu lưu trữ
+    bool deleted;                     //> Trạng thái xoá hay chưa (xoá quản lí, cho phương thức soft-delete)
 } Record;
 
 /// @brief một node quản lí cơ bản
 typedef struct Node
 {
-    void **pointers;     // dùng để lưu trữ dữ liệu (Record), hoặc các node con
-    int *keys;           // lưu trữ các khóa, định hướng quá trình tìm kiếm
-    struct Node *parent; // Node tổ tiên, kết nối dùng để quản lí
-    bool is_leaf;        // Là node lá hay không, chỉ thị trạng thái dùng cho liên kết
-    int num_keys;        // Số lượng khóa
-    struct Node *next;   // Trỏ tiếp theo (phục vụ enqueue, dequeue)
+    void **pointers;     //> dùng để lưu trữ dữ liệu (Record), hoặc các node con
+    int *keys;           //> lưu trữ các khóa, định hướng quá trình tìm kiếm
+    struct Node *parent; //> Node tổ tiên, kết nối dùng để quản lí
+    bool is_leaf;        //> Là node lá hay không, chỉ thị trạng thái dùng cho liên kết
+    int num_keys;        //> Số lượng khóa
+    struct Node *next;   //> Trỏ tiếp theo (phục vụ enqueue, dequeue)
 } Node;
 
+/**
+ * @brief Khởi tạo record
+ *
+ * Tạo dựng record mới từ
+ * @param _from
+ * @param offset
+ * @param length
+ */
 Record *makeRecord(char _from[MAX_FILE_NAME_LENGTH], long offset, long length);
+
+/**
+ * @brief hàm trả về node lá tận cùng bên trái
+ *
+ * Hàm này được dùng cho nhóm tác vụ liên quan đến thống kê, báo cáo
+ * Yêu cầu duyệt toàn bộ nội dung file, số sách đã lưu trữ, ...
+ *
+ * @param root, node gốc cần quản lí
+ */
 Node *leftMost(Node *root);
+
+/**
+ * @brief Khởi tạo một node
+ */
 Node *makeNode(void);
+
+/**
+ * @brief Khởi tạo một node lá
+ *
+ * Bản chất là một tinh chỉnh nhỏ của makeNode, thông qua thay đổi thuộc tính is_leaf
+ */
 Node *makeLeaf(void);
 
 /// Phương thức tìm kiếm
+
+/**
+ * @brief Tìm kiếm node lá tương ứng với Key
+ *
+ * Dùng phương pháp duyệt cây tương tự như trong cây tìm kiếm nhị phân
+ * @param root, node gốc
+ * @param key, khoá tìm kiếm
+ */
 Node *findLeaf(Node *root, int key);
+
+/**
+ * @brief Tìm kiếm bản ghi từ Key
+ *
+ * Bản chất là một kế thừa mở rộng từ findLeaf
+ * @param root, node gốc
+ * @param key, khoá tìm kiếm
+ */
 Record *find(Node *root, int key);
 
 /// Phương thức bổ trợ
+
+/**
+ * @brief Hàm hỗ trợ cho việc chia tách node, dành cho trường hợp yêu cầu chia tách
+ *
+ * Cách chia tách sẽ khác nhau tuỳ theo đầu vào là chẵn hay lẻ
+ * @param length, số bậc (dựa trên order)
+ */
 int cut(int length);
+
+/**
+ * @brief Hàm hỗ trợ cho việc trả về vị trí bắt đầu chỉ số bên trái đầu tiên của node
+ * được chỉ định
+ *
+ * @param parent, node cha
+ * @param left, node left, node bên trái
+ *
+ * @return vị trí bắt đầu chỉ số bên trái đầu tiên
+ */
 int getLeftIndex(Node *parent, Node *left);
 
 #ifdef PRINT_TREE
@@ -67,73 +133,50 @@ void printTree(Node *const root);
 /// @return Node gốc
 Node *startNewTree(int key, Record *pointer);
 
-/// @brief hỗ trợ insert, insert vào node lá
-/// @param leaf (lá)
-/// @param key khoá tìm kiếm
-/// @param record bản ghi dữ liệu
-/// @return
-Node *insertIntoLeaf(Node *leaf, int key, Record *record);
+// Node *insertIntoLeaf(Node *leaf, int key, Record *record);
+// Node *insertIntoNode(Node *root, Node *n, int left_index, int key, Node *right);
+// Node *insertIntoNodeAfterSplitting(Node *root, Node *old_node, int left_index, int key, Node *right);
+// Node *insertIntoNewRoot(Node *left, int key, Node *right);
+// Node *insertIntoParent(Node *root, Node *left, int key, Node *right);
+// Node *insertIntoLeafAfterSplitting(Node *root, Node *leaf, int key, Record *record);
 
-/// @brief hỗ trợ insert
-/// @param root
-/// @param n
-/// @param left_index
-/// @param key
-/// @param right
-/// @return
-Node *insertIntoNode(Node *root, Node *n, int left_index, int key, Node *right);
-
-/// @brief hỗ trợ insert
-/// @param root
-/// @param old_node
-/// @param left_index
-/// @param key
-/// @param right
-/// @return
-Node *insertIntoNodeAfterSplitting(Node *root, Node *old_node, int left_index, int key, Node *right);
-
-/// @brief hỗ trợ insert
-/// @param left
-/// @param key
-/// @param right
-/// @return
-Node *insertIntoNewRoot(Node *left, int key, Node *right);
-
-/// @brief hỗ trợ insert
-/// @param root
-/// @param left
-/// @param key
-/// @param right
-/// @return
-Node *insertIntoParent(Node *root, Node *left, int key, Node *right);
-
-/// @brief hỗ trợ insert
-/// @param root
-/// @param leaf
-/// @param key
-/// @param record
-/// @return
-Node *insertIntoLeafAfterSplitting(Node *root, Node *leaf, int key, Record *record);
-
-/// @brief Hàm insert, thêm dữ liệu lưu trữ vào quản lí, thêm vào cây B+
-/// @param root cây quản lí
-/// @param key khoá dùng để tìm kiếm, trường hợp đã tồn tại, cập nhật
-/// @param _from tên file nội dung lưu trữ
-/// @param offset vị trí bắt đầu lưu trữ trong file
-/// @param length kích thước dữ liệu
-/// @return
+/**
+ * @brief Hàm hỗ trợ cho việc thêm phần tử vào cây
+ *
+ * Thêm phần tử vào cây sẽ thêm nội dung vào node lá, và các node không là node lá sẽ chứa dữ liệu
+ * định hướng tìm kiếm.
+ *
+ * @param root, node gốc ban đầu
+ * @param key, khoá tìm kiếm
+ * @param _from, file nội dung lưu trữ
+ * @param offset, vị trí bắt đầu lưu trữ trong file
+ * @param length, kích thước dữ liệu
+ *
+ * @return cây mới sau khi thêm
+ */
 Node *insert(Node *root, int key, char _from[MAX_FILE_NAME_LENGTH], long offset, long length);
 
 /// Phương thức lưu trữ
 
-/// @brief Lưu file
-/// @param root cây lưu trữ
-/// @param filename tên file muốn lưu trữ (đuôi .bin)
+/**
+ * @brief Hàm hỗ trợ lưu trữ dữ liệu vào file
+ *
+ * File được lưu dưới dạng đuôi .bin
+ * Dữ liệu được thành một mảng byte liên tiếp
+ *
+ * @param root, tượng trưng cho cây muốn lưu trữ
+ * @param filename tên file muốn lưu trữ
+ */
 void saveTree(Node *root, char *filename);
 
-/// @brief Load cây
-/// @param filename tên file
-/// @return
+/**
+ * @brief Hàm hỗ trợ lấy dữ liệu ra từ cây
+ *
+ * Việc tải nội dung được tiến hành tương ứng thông qua hình
+ * thức lưu trữ
+ *
+ * @param filename file đã lưu trữ trước đó
+ */
 Node *loadTree(char *filename);
 
 #endif
