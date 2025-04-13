a2bc6f63b25ff542e24e48511843f4da2646777b
baolam <lam01662052827@gmail.com>
2025-04-13 11:32:45 +0700

    Add documentation

---
+++ document/html/borrow__return_8h.js
@@ -0,0 +1,20 @@
+var borrow__return_8h =
+[
+    [ "BorrowReturn", "struct_borrow_return.html", "struct_borrow_return" ],
+    [ "MAX_BORROWED_BOOKS", "borrow__return_8h.html#a0952e94a037eac280b9ee1b49d3e95cf", null ],
+    [ "BorrowReturn", "borrow__return_8h.html#ab73b402c920f03bb44641490f56ba94d", null ],
+    [ "add_borrow_record", "borrow__return_8h.html#acd1efdcb57a19296d6fd30d547d47bfa", null ],
+    [ "check_book_in_borrow", "borrow__return_8h.html#a0debb124a322065a6209e63b9e56a2a4", null ],
+    [ "delete_borrow_record", "borrow__return_8h.html#a2b6bda3e39faebd850ff3488c25cc648", null ],
+    [ "load_borrow_return_management", "borrow__return_8h.html#a3f6198ab52b858e20c54463b4d00e4d2", null ],
+    [ "restore_books_to_stock", "borrow__return_8h.html#a8c3fff20022a9308a55170a9855dbd6a", null ],
+    [ "return_books", "borrow__return_8h.html#a6d1031f3d3de9268eea5484521c2096f", null ],
+    [ "save_borrow_return_management", "borrow__return_8h.html#ae46ecefa1043dad90969d2bc3f81626a", null ],
+    [ "search_borrow_record_by_reader", "borrow__return_8h.html#ae3ddf26aa8c6f214f485e638682f20b2", null ],
+    [ "show_borrow_record", "borrow__return_8h.html#ab19475356baa01c670623e0c95d998fa", null ],
+    [ "stat_total_books_by_reader", "borrow__return_8h.html#a961ad1c096b0a10c15765365130eb796", null ],
+    [ "update_book_direct", "borrow__return_8h.html#a5c24caa6c8dced26c0dcf2434a78bc9d", null ],
+    [ "borrow_return_content_file", "borrow__return_8h.html#a04f30d552462012a85063968ed30242f", null ],
+    [ "borrow_return_management", "borrow__return_8h.html#a974b0a5ef39ead4f57706dd0555b4aca", null ],
+    [ "borrow_return_management_file", "borrow__return_8h.html#a09c52186588892bacc969675814632ae", null ]
+];
---
+++ document/html/search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['borrowreturn_0',['BorrowReturn',['../borrow__return_8h.html#ab73b402c920f03bb44641490f56ba94d',1,'borrow_return.h']]]
+];
