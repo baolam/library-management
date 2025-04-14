412f372b17475b79aa5dd0cb8fc79d10afb413a8
ducphucdn <phamducphuc07.10.2006@gmail.com>
2025-04-14 11:44:20 +0700

    p

--- src/gui/events/book_treeview.c
+++ src/gui/events/book_treeview.c
@@ -5,37 +5,52 @@
 
 int currnent_page = 1;
 
-void load_book_to_layout(GtkBuilder *builder, int beginingKey)
+void show_onebook_to_layout(GtkListStore *store, Book book)
+{
+    gchar *bookId = g_strdup_printf("%d", book.bookId);
+    gchar *year = g_strdup_printf("%d", book.publicationYear);
+    gchar *stock = g_strdup_printf("%d", book.stock);
+
+    GtkTreeIter iter;
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter,
+                       0, bookId,
+                       1, book.title,
+                       2, book.author,
+                       3, book.genre,
+                       4, year,
+                       5, stock,
+                       -1);
+
+    g_free(bookId);
+    g_free(year);
+    g_free(stock);
+}
+
+GtkListStore *reference_store()
 {
     GtkListStore *store = GTK_LIST_STORE(gtk_builder_get_object(builder, "liststore1"));
     gtk_list_store_clear(store);
+    return store;
+}
 
-    int size = 0;
-    Book *book = retrieve_bucket_books(beginingKey, MAX_ROW_ONEPAGE, &size);
+void show_book_to_layout(Book *book, int size)
+{
+    GtkListStore *store = reference_store();
 
     int i;
     for (i = 0; i < size; i++)
     {
-        gchar *bookId = g_strdup_printf("%d", book[i].bookId);
-        gchar *year = g_strdup_printf("%d", book[i].publicationYear);
-        gchar *stock = g_strdup_printf("%d", book[i].stock);
-
-        GtkTreeIter iter;
-        gtk_list_store_append(store, &iter);
-        gtk_list_store_set(store, &iter,
-                           0, bookId,
-                           1, book[i].title,
-                           2, book[i].author,
-                           3, book[i].genre,
-                           4, year,
-                           5, stock,
-                           -1);
-
-        g_free(bookId);
-        g_free(year);
-        g_free(stock);
-                                          
+        show_onebook_to_layout(store, book[i]);                                     
     }
+}
+
+void load_book_to_layout(int beginingKey)
+{
+
+    int size = 0;
+    Book *book = retrieve_bucket_books(beginingKey, MAX_ROW_ONEPAGE, &size);
+    show_book_to_layout(book, size);
 }
 
 void on_previous_page1_clicked(GtkWidget *widget, gpointer user_data)
@@ -43,12 +58,12 @@ void on_previous_page1_clicked(GtkWidget *widget, gpointer user_data)
     if (currnent_page > MAX_ROW_ONEPAGE)
     {
         currnent_page-=MAX_ROW_ONEPAGE;
-        load_book_to_layout(builder, currnent_page);
+        load_book_to_layout(currnent_page);
     }
 }
 
 void on_next_page1_clicked(GtkWidget *widget, gpointer user_data)
 {
     currnent_page+=MAX_ROW_ONEPAGE;
-    load_book_to_layout(builder, currnent_page);
+    load_book_to_layout(currnent_page);
 }
--- src/gui/gui.c
+++ src/gui/gui.c
@@ -13,7 +13,7 @@ void init_gui(int argc, char *argv[])
     builder = load_ui("ui/UI.glade");
     GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
 
-    load_book_to_layout(builder, 1);
+    load_book_to_layout(1);
     gtk_builder_connect_signals(builder, NULL);
 
     popup_menu_init(builder);
